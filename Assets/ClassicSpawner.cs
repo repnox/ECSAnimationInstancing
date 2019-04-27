3ad5af49f900bcbaadc9be23d0e0eeb31330313c
James <james.watkins.2009@gmail.com>
2019-04-27 11:59:41 -0400

    Pure ECS Instanced rendering with Mummies

---
+++ Assets/ClassicSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassicSpawner : MonoBehaviour
+{
+
+    public GameObject prefab;
+
+    public int numX;
+
+    public int numY;
+
+    public float gap;
+
+    void Start()
+    {
+        for (int x = 0; x < numX; x++)
+        {
+            for (int y = 0; y < numY; y++)
+            {
+                Instantiate(prefab, new Vector3(transform.position.x + gap * x, transform.position.y, transform.position.z + gap * y),
+                    Quaternion.AngleAxis(Random.value * 360f, Vector3.up));
+            }
+        }
+    }
+}
